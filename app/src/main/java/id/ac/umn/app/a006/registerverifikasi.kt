416a6d0946a21b5fde822f413bb5f6d2c447e7e4
Gerardo Jason <gerardoojason@gmail.com>
2022-05-27 10:33:14 +0700

    second commit

---
+++ app/src/main/java/id/ac/umn/app/a006/registerverifikasi.kt
@@ -0,0 +1,24 @@
+package id.ac.umn.app.a006
+
+import android.content.Intent
+import android.widget.Toast
+
+object registerverifikasi {
+        private val user = "abc"
+
+        fun validpassword(
+            password : String,
+            confirmpassword : String
+        ):Boolean{
+            if (password.isEmpty()|| confirmpassword.isEmpty()){
+                return false
+            }
+            if (password !=confirmpassword){
+                return false
+            }
+            if (password.count(){it.isDigit()} <8){
+                return false
+            }
+            return true
+        }
+    }
